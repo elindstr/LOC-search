ede928abcdf0de9f54a4589bae9373fa4d2b25cf
elindstr <elindstr@gmail.com>
2024-01-24 21:29:30 -0800

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ $("#searchButton").on("click", (event) => {
     event.preventDefault()
     let searchQuery = $("#searchQuery").val()
     let mediaType = $(".form-select").val()
-    let urlParameters = `?searchQuery='${searchQuery}'&mediaType='${mediaType}'`
+    let selectorMediaType = $(".form-select").attr("data-value")
+    let urlParameters = `?searchQuery='${searchQuery}'&mediaType='${mediaType}'&mediaTypeInput='${selectorMediaType}'`
 
     let html = document.location.pathname.split("/")
     let htmlFile = html[html.length - 1]
@@ -26,9 +27,12 @@ $("#searchButton").on("click", (event) => {
 // get search params on load
 $(function() {
     let params = new URLSearchParams(window.location.search);
-    let searchQuery = params.get('searchQuery');
-    let mediaType = params.get('mediaType');
-    //console.log(searchQuery, mediaType)
+    let searchQuery = params.get('searchQuery').replaceAll("'","")
+    let mediaType = params.get('mediaType').replaceAll("'","")
+    console.log(searchQuery, mediaType)
+
+    $("#searchQuery").val(`${searchQuery}`)
+    $("#mediaType").val(`${mediaType}`)      // TODO: Debug - not working. 
 
     searchQuery? fetchQuery(searchQuery, mediaType): null
 })
@@ -37,7 +41,7 @@ $(function() {
 function fetchQuery(searchQuery, mediaType) {
     let fetchURL = `https://www.loc.gov/search/?q=${searchQuery}&fo=json`
     console.log(mediaType)
-    if (mediaType !== "Any Format") { //TODO: Why isn't this working?
+    if (mediaType !== "Any Format") {
         fetchURL = `https://www.loc.gov/search/?q=${searchQuery}&fa=${mediaType}&fo=json`
     }
     console.log("fetchURL:", fetchURL)
@@ -69,9 +73,11 @@ function renderResults(data) {
 
         if (data.results) {
             let resultsLength = data.results.length
-            
-            let resultsLengthMsg = `Showing ${resultsLength} results:`
-            resultsLength === 1? resultsLengthMsg `Showing ${resultsLength} result:`: null   
+            let ofCount = data.pagination.of
+            console.log(ofCount)
+
+            let resultsLengthMsg = `Showing ${resultsLength} of ${ofCount.toLocaleString()} results:`
+            resultsLength === 1? resultsLengthMsg `Showing ${resultsLength} of ${ofCount} result:`: null   
             $("main header").append("<h2>")
                 .text(resultsLengthMsg) 
 
@@ -85,18 +91,42 @@ function renderResults(data) {
                         .text(sectionHeadContent)
                 }
 
-                let sectionBodyContent = data.results[i].description[0]
-                let sectionBody = $(`<p>`)
-                    .text(sectionBodyContent)
+                let image
+                if (data.results[i].image_url) {
+                    image = $("<img>")
+                    .attr("src", data.results[i].image_url[0])
+                }
 
-                let sectionButton = $("<button>")
+
+                let dateDiv = $("<div>")
+                let dateLabel = $("<span>Date:</span>").attr("class", "bold")
+                let dateContent = `<span>${data.results[i].date}</span>`
+                dateDiv.append(dateLabel, dateContent)
+
+                let descriptionDiv = $("<div>")
+                let descriptionLabel = $("<span>Description:</span>").attr("class", "bold")
+                let descriptionContent = `<span>${data.results[i].description[0]}</span>`
+                descriptionDiv.append(descriptionLabel, descriptionContent)
+
+                let sectionButton = $("<a>")
                     .text("View More")
-                    .attr("data-url", `${data.results[i].url}`)
                     .attr("class", "btn btn-primary")
+                    .attr("href", `${data.results[i].url}`)
 
-                section.append(sectionHead, sectionBody, sectionButton)
+                section.append(sectionHead, image, dateDiv, descriptionDiv, sectionButton)
 
                 $("article").append(section)
+            }
+
+            // next page
+            if (data.pagination.next) {
+                let next = data.pagination.next
+                let last = data.pagination.last
+                let of = data.pagination.of
+                $("article").append($("<button>")
+                    .text("Next")
+                    .attr("id", next)
+                    .addClass("next"))
             }
         }
         else {
@@ -107,4 +137,48 @@ function renderResults(data) {
     else {
         console.log("no data to render")
     }
-}          
+}
+
+$("body").on("click", "button .next", function () {
+    url = $(this).attr("id")
+    console.log("click", url)
+
+    fetch(url)
+        .then(function (response) {
+            if (response.status >= 400) {
+                console.log("fetch error")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+            renderResults(data)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+})
+
+$("body").on("click", "button .next", function () {
+    url = $(this).attr("id")
+    console.log("click", url)
+
+    fetch(url)
+        .then(function (response) {
+            if (response.status >= 400) {
+                console.log("fetch error")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+            renderResults(data)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+})
